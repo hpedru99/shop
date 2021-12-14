b4108d45089f73b790a07e3e7470233b8f059a18
hpedru99 <pedrohelk@gmail.com>
2021-12-14 16:27:12 +0300

    Исправлена табуляция

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -5,7 +5,9 @@ from .models import Category, Product
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
-    admin.site.register(Category, CategoryAdmin)
+
+
+admin.site.register(Category, CategoryAdmin)
 
 
 class ProductAdmin(admin.ModelAdmin):
